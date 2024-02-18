45f90b7e03e604927f2c55e73b96d291eedc3b71
Utkarsh-Singh <utkarshsp206@gmail.com>
2024-02-18 20:23:31 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); //performs type conversion first
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  Strict equality check, it also checks the datatype and does not do type conversion
+
+console.log("2" === 2);
