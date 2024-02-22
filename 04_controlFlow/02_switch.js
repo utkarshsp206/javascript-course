013d4b4ed23868d738a3df51d36822914688893e
Utkarsh-Singh <utkarshsp206@gmail.com>
2024-02-22 17:21:10 +0530

    Conditionals, Switch, Truthy, Nullish Coalescing Op

---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,24 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const num = 2
+switch(num){
+    case 1:
+        console.log("Num value is 1");
+        break;
+    
+    case 2:
+        console.log("Num value is 2");
+        break;
+    
+    default:
+        console.log("Num value not among 1 or 2");
+        break;
+        
+}
